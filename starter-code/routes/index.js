905dd842920de7e3cb94588d873ee483473f4f25
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-04-13 09:22:11 +0200

    segunda iteración

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/login', loginController.login)
+router.post('/', loginController.doLogin)
 
 module.exports = router;
